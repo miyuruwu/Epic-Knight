92826210cdc7d48b76dfb7132d569887cf57b558
Yuuk1 <dohoangkhoa1@gmail.com>
2025-02-24 20:24:29 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,38 @@
-#include<SDL3/SDL.h>
-#include<SDL3_image/SDL_image.h>
-#include<iostream>
-using namespace std;
+#define SDL_MAIN_HANDLED
+#include<SDL2/SDL.h>
+#include<SDL2/SDL_image.h>
+#include <iostream>
+
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
+
+void initialize() {
+    if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
+        exit(1);
+    }
+    window = SDL_CreateWindow("Epic Knight v1.0", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    if(window == nullptr) {
+        std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
+        exit(1);
+    }
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if(renderer == nullptr) {
+        std::cerr << "Failed to create renderer: " << SDL_GetError() << std::endl;
+        exit(1);
+    }
+}
+
+void close() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
 
 int main(int argc, char* argv[]) {
-    if(SDL_Init(SDL_INIT_VIDEO) > 0) {
-        cerr << "SDL_Init has failed. SDL_Error: " << SDL_GetError() << endl;
-        return 1;
-    }
-    if(!(IMG_Init(IMG_INIT_PNG))) {
-        cerr << "IMG_Init has failed. Error: " << SDL_GetError() << endl;
-    }
-    RenderWindow window("Epic Knight v1.0", 1280, 720);
+    initialize();
+    close();
+    return 0;
 }
